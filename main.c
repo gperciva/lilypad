74d4a4a7fb78f6400552699447225d2fb2af869b
Patrick McCarty <pnorcks@gmail.com>
2010-07-06 01:15:32 -0700

    Use `Courier New' as the default monospace font.

--- main.c
+++ main.c
@@ -55,20 +55,20 @@ VOID SetFileName(__LPCWSTR szFileName)
 static VOID LILYPAD_InitFont()
 {
     LOGFONT *lf = &Globals.lfFont;
-    static const __WCHAR systemW[] = { 'S','y','s','t','e','m',0 };
+    static const __WCHAR systemW[] = { 'C','o','u','r','i','e','r',' ','N','e','w',0 };
 
-    lf->lfHeight        = -10;
+    lf->lfHeight        = -13;
     lf->lfWidth         = 0;
     lf->lfEscapement    = 0;
     lf->lfOrientation   = 0;
-    lf->lfWeight        = FW_BOLD;
+    lf->lfWeight        = FW_NORMAL;
     lf->lfItalic        = FALSE;
     lf->lfUnderline     = FALSE;
     lf->lfStrikeOut     = FALSE;
     lf->lfCharSet       = DEFAULT_CHARSET;
     lf->lfOutPrecision  = OUT_DEFAULT_PRECIS;
     lf->lfClipPrecision = CLIP_DEFAULT_PRECIS;
-    lf->lfQuality       = DEFAULT_QUALITY;
+    lf->lfQuality       = ANTIALIASED_QUALITY;
     lf->lfPitchAndFamily = FIXED_PITCH | FF_DONTCARE;
     lstrcpy(lf->lfFaceName, systemW);
 
