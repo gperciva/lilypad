2a887c3d6cbcf6d51768df902740f38792f1ad8c
Marnen Laibow-Koser <marnen@marnen.org>
2020-01-08 16:53:44 +0000

    Change path for Python 2.7 in venv.

--- macosx/setup.py
+++ macosx/setup.py
@@ -37,7 +37,7 @@ setup(
 		'ProcessLog.nib',
 		'lilycall.py',
 		'ProcessLog.py',
-		'/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/shutil.py',
+                'venv/lib/python2.7/site-packages/pip/_vendor/distlib/_backport/shutil.py',
 		'Credits.html',
 		'URLHandle.scriptSuite',
 		'URLHandle.scriptTerminology',
